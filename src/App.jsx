1377fade456068db1cea6ddaaa7dd547032d2608
myk3l <ayomikesam@gmail.com>
2025-10-25 21:52:32 +0100

    Remove Vercel Analytics and update styles and HTML structure for the TACN Yaba District project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 import "./App.css";
-import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <>
       <AppRoutes />
-      <Analytics />
     </>
   );
 }
